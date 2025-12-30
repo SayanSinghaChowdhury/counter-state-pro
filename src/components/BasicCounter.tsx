cc78296cdf8860bc509318b692d11ae275540459
Sayan Singha Chowdhury <sayansinghachowdhury.dev@gmail.com>
2025-12-30 15:54:16 +0530

    Basic part logic done use state local way✌️

--- src/components/BasicCounter.tsx
+++ src/components/BasicCounter.tsx
@@ -1,21 +1,35 @@
+"use client";
+
 import { CircleMinus, CirclePlus } from "lucide-react";
+import { useState } from "react";
 import { Button } from "./shadcnui/button";
 
 const BasicCounter = () => {
+	const [counter, setcounter] = useState(0);
+
+	const plus = () => {
+		setcounter(counter + 1);
+	};
+	const minus = () => {
+		setcounter(counter - 1);
+	};
+
 	return (
 		<div className="grid place-items-center gap-4">
 			<h1 className="text-lg">
-				Counter Value: <span className="font-semibold">0</span>
+				Counter Value: <span className="font-semibold">{counter}</span>
 			</h1>
 
 			<div className="grid grid-cols-2 gap-4">
 				<Button
+					onClick={minus}
 					variant={"destructive"}
 					className="cursor-pointer">
 					<CircleMinus /> Minus
 				</Button>
+
 				<Button
-					variant={"default"}
+					onClick={plus}
 					className="cursor-pointer">
 					<CirclePlus /> Plus
 				</Button>
